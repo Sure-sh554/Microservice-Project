bca378ea962663da36ddc90b882ed8c1ade0ac73
Sure-sh554 <sureshrathod0113@gmail.com>
2024-07-04 20:07:28 +0530

    Department_Service

--- Department-Servie/src/main/java/com/microservice/Department/Servie/exception/GlobalExceptionHandler.java
+++ Department-Servie/src/main/java/com/microservice/Department/Servie/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -20,6 +21,20 @@ public class GlobalExceptionHandler {
 
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleException(Exception exception,
+                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL_SERVER_ERROR"
+
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
